57022a5f6d397b6a6278a54b1496e86fad65cabd
Luiz Reginaldo Silva <contato@luizreginaldo.com>
2021-11-17 19:56:56 -0300

    create topic

--- src/main/kotlin/br/com/alura/forum/controller/TopicController.kt
+++ src/main/kotlin/br/com/alura/forum/controller/TopicController.kt
@@ -2,10 +2,7 @@ package br.com.alura.forum.controller
 
 import br.com.alura.forum.model.Topic
 import br.com.alura.forum.service.TopicService
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/topics")
@@ -21,6 +18,11 @@ class TopicController(
     @GetMapping("/{id}")
     fun finById(@PathVariable id: Long) : Topic {
         return service.findById(id)
+    }
+
+    @PostMapping
+    fun create(@RequestBody topic: Topic) {
+        service.create(topic)
     }
 
 }
--- src/main/kotlin/br/com/alura/forum/service/TopicService.kt
+++ src/main/kotlin/br/com/alura/forum/service/TopicService.kt
@@ -7,60 +7,8 @@ import org.springframework.stereotype.Service
 
 @Service
 class TopicService(
-    private var topics: List<Topic>
+    private var topics: List<Topic> = listOf()
 ) {
-
-    init {
-        val topic = Topic(
-            id = 1,
-            title = "Kotlin doubt 1",
-            message = "Variables in Kotlin 1",
-            course = Course(
-                id = 1,
-                name = "Kotlin",
-                category = "Programming"
-            ),
-            author = User(
-                id = 1,
-                name = "Luiz Reginaldo",
-                email = "contato@luizreginaldo.com"
-            )
-        )
-
-        val topic2 = Topic(
-            id = 2,
-            title = "Kotlin doubt 2",
-            message = "Variables in Kotlin 2",
-            course = Course(
-                id = 1,
-                name = "Kotlin",
-                category = "Programming"
-            ),
-            author = User(
-                id = 1,
-                name = "Luiz Reginaldo",
-                email = "contato@luizreginaldo.com"
-            )
-        )
-
-        val topic3 = Topic(
-            id = 3,
-            title = "Kotlin doubt 3",
-            message = "Variables in Kotlin 3",
-            course = Course(
-                id = 1,
-                name = "Kotlin",
-                category = "Programming"
-            ),
-            author = User(
-                id = 1,
-                name = "Luiz Reginaldo",
-                email = "contato@luizreginaldo.com"
-            )
-        )
-
-        topics = listOf(topic, topic2, topic3)
-    }
 
     fun list(): List<Topic> {
         return topics
@@ -70,6 +18,10 @@ class TopicService(
         return topics.stream().filter { topic ->
             topic.id == id
         }.findFirst().get()
+    }
+
+    fun create(topic: Topic) {
+        topics.plus(topic)
     }
 
 }
