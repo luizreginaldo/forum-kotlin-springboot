bab27aa358812d63c3ccf15fced91e07f8853cc7
Luiz Reginaldo Silva <contato@luizreginaldo.com>
2021-11-20 11:09:24 -0300

    mapping JPA entities

--- src/main/kotlin/br/com/alura/forum/model/Answer.kt
+++ src/main/kotlin/br/com/alura/forum/model/Answer.kt
@@ -1,12 +1,21 @@
 package br.com.alura.forum.model
 
 import java.time.LocalDateTime
+import javax.persistence.*
 
+@Entity
 data class Answer(
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     val id: Long?,
+
     val message: String,
     val createdAt: LocalDateTime = LocalDateTime.now(),
+
+    @ManyToOne
     val author: User,
+
+    @ManyToOne
     val topic: Topic,
+
     val solution: Boolean
 )
--- src/main/kotlin/br/com/alura/forum/model/Topic.kt
+++ src/main/kotlin/br/com/alura/forum/model/Topic.kt
@@ -1,14 +1,26 @@
 package br.com.alura.forum.model
 
 import java.time.LocalDateTime
+import javax.persistence.*
 
+@Entity
 data class Topic(
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     var id: Long? = null,
+
     var title: String,
     var message: String,
     val createdAt: LocalDateTime = LocalDateTime.now(),
+
+    @ManyToOne
     val course: Course,
+
+    @ManyToOne
     val author: User,
+
+    @Enumerated(value = EnumType.STRING)
     val status: TopicStatus = TopicStatus.UNANSWERED,
+
+    @OneToMany(mappedBy = "topic")
     val answers: List<Answer> = ArrayList()
 )
